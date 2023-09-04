6facb2c1883a69206f4ae643388a948ef9ea8ff1
Xillez <kent.wincent46@gmail.com>
2023-09-04 15:22:28 +0200

    synced up to task 1 and cleaned up, moved task 2 solution to proper place

---
+++ highcharts-api/highcharts/2-polar-rings/main.js
@@ -0,0 +1,83 @@
+function generateRandomData(amount, max) {
+    return Array.from({ length: amount }, () => Math.floor(Math.random() * max));
+}
+
+Highcharts.chart('container', {
+    chart: {
+        type: 'column',
+        polar: true,
+        events: {
+            load: function () {
+                // Find the highest value among the series.
+                const max = this.yAxis[0].dataMax;
+                this.update({
+                    yAxis: {
+                        max: 2 * max,
+                        plotLines: [
+                            {
+                                value: 1.5 * max,
+                                dashStyle: 'Dash',
+                                color: '#73FF50',
+                                width: 3
+                            },
+                        ]
+                    }
+                });
+                this.yAxis[0].addPlotLine({
+                    id: '1.75*max',
+                    value: 1.75 * max,
+                    dashStyle: 'Solid',
+                    color: '#FD7E7E'
+                });
+                this.yAxis[0].addPlotLine({
+                    id: 'max',
+                    value: max,
+                    dashStyle: 'Dash',
+                    color: '#43BDFF6C'
+                });
+            }
+        }
+    },
+    title: {
+        text: ''
+    },
+    xAxis: {
+        categories: ['Jan', 'Feb', 'Mar'],
+    },
+    yAxis: {
+        title: {
+            enabled: false
+        }
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+        },
+        series: {
+            dataLabels: {
+                enabled: true,
+                formatter: function () {
+                    return this.series.yAxis.dataMax == this.y ? 'max' : '';
+                }
+            }
+        }
+    },
+    series: [
+        {
+            name: 'Tokyo',
+            type: 'column',
+            data: generateRandomData(3, 10)
+        },
+        {
+            name: 'New York',
+            type: 'column',
+            data: generateRandomData(3, 10)
+        },
+        {
+            name: 'London',
+            type: 'column',
+            data: generateRandomData(3, 10)
+        }
+    ]
+});
