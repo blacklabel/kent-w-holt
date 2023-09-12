e81a25552796df0aee18f25b29bd4f6134dbf59b
Xillez <kent.wincent46@gmail.com>
2023-09-12 12:43:07 +0200

    feedback task 4

--- highcharts-api/highcharts/4-separate-axes/main.js
+++ highcharts-api/highcharts/4-separate-axes/main.js
@@ -2,6 +2,27 @@ function generateRandomData(amount, max) {
     return Array.from({ length: amount }, () => Math.floor(Math.random() * max));
 }
 
+function getValueOnAxis(axis, val) {
+    return axis.len * (val / axis.max);
+}
+
+function makeTitles(chart, renderer, leftAxis, rightAxis) {
+    // Make the shadows container.
+    if (!chart.titles) {
+        chart.titles = {
+            leftAxisText: renderer.text(
+                'Manegrial Position',
+            ).add(),
+            rightAxisText: renderer.text(
+                'Non Manegrial Position'
+            ).add()
+        };
+    }
+
+    centerLabelOnAxis(chart.titles.leftAxisText, leftAxis);
+    centerLabelOnAxis(chart.titles.rightAxisText, rightAxis);
+}
+
 function centerLabelOnAxis(label, axis) {
     const leftAxisTextBB = label.getBBox();
     // Position label horizontally centered on the given axis
@@ -16,8 +37,52 @@ function centerLabelOnAxis(label, axis) {
     label.attr({ x: leftTextPosX, y: leftTextPosY });
 }
 
+function makeShadows(chart, renderer, xAxis, leftAxis, rightAxis) {
+    // Clean up previous shadows if any.
+    if (chart.shadows?.length > 0) {
+        for (const shadow of chart.shadows) {
+            shadow.destroy();
+        }
+    }
+
+    // Clears any old shadows.
+    chart.shadows = [];
+    
+    // Loops through all yAxis' points and adds shadows.
+    for (const [i, axis] of [leftAxis, rightAxis].entries()) {
+        for (const point of axis.series[0].points) {
+            // Point's position on the current y-axis in pixels.
+            const valueAxisPosX = getValueOnAxis(axis, point.y);
+    
+            // Position on the end of either axis.
+            const x = i < 1 ? axis.left : axis.left + valueAxisPosX - 1;
+            const y =
+            // Add spacing offset +
+            chart.spacingBox.y +
+            // Add on the axis point padding +
+            (xAxis.minPixelPadding / 2) +
+            // Get where the point should be along the raw un-padded axis
+            // divided by categories +
+            ((xAxis.len / categories.length) * point.x);
+            const width =
+                // Start where value 100 is on the axis -
+                getValueOnAxis(axis, 100) -
+                // Get where the current point should be along the raw un-padded
+                // axis divided by categories +
+                valueAxisPosX +
+                // Shadow is one pixel off on the right side (i = 1 on right y-axis).
+                i;
+            const height = point.pointWidth;
+            const shadow = renderer
+                .rect(x, y, width, height)
+                .attr({ fill: '#B6B6B6', zIndex: 1 })
+                .add();
+            chart.shadows.push(shadow);
+        }
+    }
+}
+
 const categories = ['Dep1', 'Dep2', 'Dep3', 'Dep4', 'Dep5'];
-let leftAxisText, rightAxisText;
 
 Highcharts.chart('container', {
     chart: {
@@ -26,18 +91,12 @@ Highcharts.chart('container', {
         events: {
             // Ready the labels once so we can re-use them.
             load: function () {
-                leftAxisText = this.renderer.text(
-                    'Manegrial Position',
-                ).add();
-                rightAxisText = this.renderer.text(
-                    'Non Manegrial Position'
-                ).add();
+                makeTitles(this, this.renderer, this.yAxis[0], this.yAxis[1]);
+                makeShadows(this, this.renderer, this.xAxis[0], this.yAxis[0], this.yAxis[1]);
             },
-            render: function () {
-                const leftAxis = this.yAxis[0];
-                const rightAxis = this.yAxis[1];
-                centerLabelOnAxis(leftAxisText, leftAxis);
-                centerLabelOnAxis(rightAxisText, rightAxis);
+            redraw: function () {
+                makeTitles(this, this.renderer, this.yAxis[0], this.yAxis[1]);
+                makeShadows(this, this.renderer, this.xAxis[0], this.yAxis[0], this.yAxis[1]);
             }
         }
     },
@@ -64,7 +123,9 @@ Highcharts.chart('container', {
             width: '40%',
             reversed: true,
             offset: 0,
-            max: 100
+            max: 100,
+            gridZIndex: 0,
+            tickInterval: 20
         },
         {
             title: {
@@ -73,7 +134,9 @@ Highcharts.chart('container', {
             left: '55%',
             width: '40%',
             offset: 0,
-            max: 100
+            max: 100,
+            gridZIndex: 0,
+            tickInterval: 20
         },
     ],
     plotOptions: {
@@ -84,21 +147,11 @@ Highcharts.chart('container', {
                 format: '{y} %',
                 inside: true,
             },
-            borderWidth: 0
+            borderWidth: 0,
+            borderRadius: 0
         }
     },
     series: [
-        // 100 percent indicator shadow
-        {
-            name: '',
-            data: Array.from({ length: categories.length }, () => 100),
-            yAxis: 0,
-            linkedTo: 'manegrial',
-            color: '#9E9FA370',
-            dataLabels: {
-                enabled: false,
-            }
-        },
         {
             id: 'manegrial',
             name: 'Manegrial Position',
@@ -107,17 +160,6 @@ Highcharts.chart('container', {
             color: '#ff0000',
             dataLabels: {
                 align: 'right',
-            }
-        },
-        // 100 percent indicator shadow
-        {
-            name: '',
-            data: Array.from({ length: categories.length }, () => 100),
-            yAxis: 1,
-            linkedTo: 'non-manegrial',
-            color: '#9E9FA370',
-            dataLabels: {
-                enabled: false,
             }
         },
         {
