c9f3e1ef559a3e27b835492271bc7bdb12ecc128
Xillez <kent.wincent46@gmail.com>
2023-10-23 15:05:14 +0200

    pr feedback

--- highcharts-api/highcharts-maps/1-points-joinby/main.js
+++ highcharts-api/highcharts-maps/1-points-joinby/main.js
@@ -2,15 +2,26 @@ Highcharts.mapChart('container', {
     chart: {
         map: 'custom/world'
     },
-    series: [{
-        data: [
-            ['POL', 100],
-            ['USA', 90],
-            ['PER', 50],
-            ['TZA', 40],
-            ['AUS', 1]
-        ],
-        keys: ['iso-a3', 'value'],
-        joinBy: ['iso-a3']
-    }]
+    series: [
+        {
+            data: [
+                ['POL', 100],
+                ['USA', 90],
+                ['PER', 50],
+                ['TZA', 40],
+                ['AUS', 1]
+            ],
+            keys: ['iso-a3', 'value'],
+            joinBy: ['iso-a3']
+        },
+        {
+            data: [
+                ['NOR', 100],
+                ['RUS', 85],
+                ['DEU', 50]
+            ],
+            keys: ['iso-a3', 'value'],
+            joinBy: ['iso-a3']
+        }
+    ]
 });
