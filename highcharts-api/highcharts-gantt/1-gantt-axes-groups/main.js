a088a734e2d2b374da0d5338632e0df6f673b67f
Xillez <kent.wincent46@gmail.com>
2023-11-07 14:23:42 +0100

    pr feedback

--- highcharts-api/highcharts-gantt/1-gantt-axes-groups/main.js
+++ highcharts-api/highcharts-gantt/1-gantt-axes-groups/main.js
@@ -1,26 +1,21 @@
 Highcharts.ganttChart('container', {
-    xAxis: {
-        labels: {
-            formatter: function () {
-                const date = new Date(this.pos);
-                return `${
-                    date.toLocaleDateString('en-GB', { weekday: 'long' })
-                }, ${
-                    date.toLocaleDateString('en-GB', { day: 'numeric', month: 'short' })
-                }, ${
-                    date.getFullYear()
-                }`;
-            }
+    xAxis: [
+        // Had to wrap axis definition in a list to get rid of extra axis label
+        // group + for dateTimeLabelFormats to work.
+        {
+            tickInterval: 7 * 24 * 3600 * 1000, // one week in milliseconds
+            dateTimeLabelFormats: {
+                week: '%A, %e %b, %Y'
+            },
         }
-    },
+    ],
     yAxis: {
         uniqueNames: true
     },
     series: [{
         name: 'Project 1',
         data: [{
-            name: 'Main',
-            pointWidth: 0
+            name: 'Main'
         }, {
             name: 'First',
             start: 1560902400000,
